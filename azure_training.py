4c9ec033033f6961fe93dbb43350f31e8787dafc
Polyethical <80647797+Polyethical@users.noreply.github.com>
2022-08-24 09:47:02 -0700

    *Original Upload

---
+++ azure_training.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+from azureml.exceptions import ComputeTargetException
+from source_code import classifier
+from azureml.core.compute import ComputeTarget, AmlCompute
+from azureml.core import Workspace
+ws = Workspace.get(name='myProject',
+               subscription_id='328f8633-9a4b-4467-8390-65e657ac9e18',
+               resource_group='ml_projects'
+               )
+
+ws.write_config(path="D:\Machine Learning Models\Machine_Learning_Projects\parkinson's_disease_detection_project", file_name="config.json")
+
+import joblib
+os.makedirs("outputs", exist_ok=True)
+joblib.dump(value=classifier, filename="outputs/pddm_cl.pkl")
+
+from azureml.core.model import Model
+model = Model.register(workspace=ws, model_path="./outputs/pddm_cl.pkl", model_name="parkinsons_disease_classifier")
+
+
+
+# Create compute target
+cpu_cluster_name = "cpu-cluster"
+
+# Verify that the cluster does not exist already
+try:
+    cpu_cluster = ComputeTarget(workspace=ws, name=cpu_cluster_name)
+    print('Found existing cluster, use it.')
+except ComputeTargetException:
+    compute_config = AmlCompute.provisioning_configuration(vm_size='STANDARD_D2_V2',
+                                                           max_nodes=4,
+                                                           idle_seconds_before_scaledown=2400)
+    cpu_cluster = ComputeTarget.create(ws, cpu_cluster_name, compute_config)
+
+cpu_cluster.wait_for_completion(show_output=True)
+
+
+# Select Compute Target
+from azureml.core.runconfig import RunConfiguration
+from azureml.core.compute import AmlCompute
+list_vms = AmlCompute.supported_vmsizes(workspace=ws)
+print(list_vms)
+compute_config = RunConfiguration()
+compute_config.target = "cpu-cluster"
+compute_config.amlcompute.vm_size = "Standard_AI_v2"
+
+# Add libraries to run model in cloud
+from azureml.core.conda_dependencies import CondaDependencies
+dependencies = CondaDependencies()
+dependencies.set_python_version("3.6.6")
+
+
+# # dependencies.set_pip_requirements(["numpy", "scikit-learn", "pandas", "matplotlib", "xgboost"])
+dependencies.add_pip_package("scikit-learn==0.20.3")
+dependencies.add_pip_package("numpy==1.16.0")
+dependencies.add_pip_package("pandas==1.1.5")
+dependencies.add_pip_package("matplotlib==3.0.0")
+dependencies.add_pip_package("xgboost==1.5.0")
+print("dependencies set")
+
+compute_config.environment.python.conda_dependencies = dependencies
+
+# Run model in the cloud
+from azureml.core.experiment import Experiment
+from azureml.core import ScriptRunConfig
+# The script you specify here is the one you wrote to train your linear model
+script_run_config = ScriptRunConfig(source_directory=".",script="source_code.py", run_config=compute_config)
+experiment = Experiment(workspace=ws, name="parkinsons_disease_classifier")
+run = experiment.submit(config=script_run_config)
+run.wait_for_completion(show_output=True)
+
+# print(run.get_file_names())
+# run.download_file(name="outputs/pddm_cl.pkl")
---
+++ source_code.py
@@ -0,0 +1,111 @@
+import os
+import pandas as pd
+# from sklearn.metrics import confusion_matrix, accuracy_score
+# from sklearn.preprocessing import StandardScaler
+import xgboost
+# from sklearn.model_selection import cross_val_score
+import numpy as np
+# import matplotlib.pyplot as plt
+import sklearn
+
+
+# Importing dataset
+dataset = pd.read_csv("parkinsons.data")
+
+# Independant and dependant variable
+X = dataset.loc[:, dataset.columns != "status"]
+y = dataset["status"].values
+
+
+# Y already binary variable, does not need scaling
+
+
+#Get names of columns for new dataframe
+names = []
+for i in list(dataset):
+    names.append(i)
+
+
+# encode dependant variable into numerical catagorical data
+
+normalized_items = []
+
+le = sklearn.preprocessing.LabelEncoder()
+names_col = le.fit_transform(X.loc[:, "name"])
+
+dataset = dataset.drop("name", axis=1)
+
+normalized_items.append(names_col)
+
+data_list = []
+
+# statuses_col = X.loc[:, "status"]
+#
+# normalized_items.append(statuses_col)
+
+
+
+# Scale independant variable within range of 0 - 1
+columns = dataset.columns
+# other_names = dataset.loc[:, dataset.columns != "status"]
+other_names = dataset
+
+dataset_items = []
+for (i, v) in other_names.iteritems():
+    dataset_items.append(v.values)
+
+def normalize_data(other_names):
+    normalized = (other_names-np.min(other_names))/(np.max(other_names)-np.min(other_names))
+    return normalized
+
+counter = 0
+for i in dataset_items:
+    # print(i)
+    if counter != 17:
+        item = normalize_data(i)
+        normalized_items.append(item)
+    counter += 1
+
+new_normalized_items = []
+for i in normalized_items:
+    new_normalized_items.append(i.tolist())
+
+items_dict = {}
+for i in new_normalized_items:
+    items_dict[names[new_normalized_items.index(i)]] = i
+
+revised_dataset = pd.DataFrame.from_dict(items_dict)
+
+
+X = revised_dataset.loc[:, revised_dataset.columns != "status"]
+y = revised_dataset["status"].values
+
+X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2, random_state=0)
+# print(X_train)
+# print(y_train)
+
+
+# Training XGBoost on the Training set
+classifier = xgboost.XGBClassifier()
+classifier.fit(X_train, y_train)
+
+# Making the Confusion Matrix
+# y_pred = classifier.predict(X_test)
+# cm = confusion_matrix(y_test, y_pred)
+# accuracy_score(y_test, y_pred)
+
+# Applying k-Fold Cross Validation
+# accuracies = cross_val_score(estimator = classifier, X=X_train, y=y_train, cv=10)
+# print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+# print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
+
+
+# TODO #1 Preproccess names into label encoded data V
+
+# TODO #2 Preproccess all other numerical data to be within range in independant variable V
+
+# TODO #3 Turn all lists into new dataset and select all values from the dataset for new X variable
+
+# TODO #4 Train model on data and try to get accuracy to 80%
+
